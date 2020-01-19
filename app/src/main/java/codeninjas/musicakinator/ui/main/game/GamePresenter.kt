549c1e178e6483e1f3442a6e8af401b4e9c8cb46
Николай Густилин <nikonic95@gmail.com>
2020-01-19 20:59:09 +0200

    add buttons

--- app/src/main/java/codeninjas/musicakinator/ui/main/game/GamePresenter.kt
+++ app/src/main/java/codeninjas/musicakinator/ui/main/game/GamePresenter.kt
@@ -1,8 +1,10 @@
 package codeninjas.musicakinator.ui.main.game
 
 import codeninjas.musicakinator.domain.models.dataModels.SongListDataModel
+import codeninjas.musicakinator.domain.usecases.GetDizzerTrackByTitleUseCase
 import codeninjas.musicakinator.other.base.BasePresenter
 import codeninjas.musicakinator.other.custom.annotations.PerFragment
+import codeninjas.musicakinator.other.custom.extensions.async
 import com.arellomobile.mvp.InjectViewState
 import javax.inject.Inject
 
@@ -11,10 +13,20 @@ import javax.inject.Inject
 class GamePresenter
 @Inject
 constructor(
-    private val songListDataModel: SongListDataModel
+    private val songListDataModel: SongListDataModel,
+    private val getDizzerTrackByTitleUseCase: GetDizzerTrackByTitleUseCase
 ) : BasePresenter<GameView>() {
 
+    private var currentSongPosition = 0
+
     init {
-        println(songListDataModel)
+        getNextTrackForListing()
+    }
+
+    fun getNextTrackForListing() {
+        getDizzerTrackByTitleUseCase.createObservable(songListDataModel.songs[currentSongPosition])
+            .async()
+            .subscribe()
+            .tracked()
     }
 }
